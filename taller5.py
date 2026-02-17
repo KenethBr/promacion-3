ca507b54bed9198ebd42d8bd91f3534f5f60064f
KenethBr <kenethstyles528@gmail.com>
2026-02-17 11:11:23 -0500

    Add "notaFinal" to taller5.py

--- taller5.py
+++ taller5.py
@@ -1,8 +1,24 @@
-#Escribe un programa en Python que solicite al usuario un número entero entre 1 y 10.
-#Luego, utiliza un bucle for para imprimir la tabla de multiplicar de ese número hasta el 10.
+# Escribe un programa en Python que solicite al usuario un número entero entre 1 y 10.
+# Luego, utiliza un bucle for para imprimir la tabla de multiplicar de ese número hasta el 10.
 
-num = int(input("Ingrese un numero del 1 al 10: "))
-#Se realiza un for para iterar del 1 al 10 para se se multiplique por el numero elegido por el usuario
-for i in range(1,11):
-    print(f"{num} x {i} = {num*i}")
 
+def tablasMultiplicar():
+    num = int(input("Ingrese un numero del 1 al 10: "))
+    # Se realiza un for para iterar del 1 al 10 para se se multiplique por el numero elegido por el usuario
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
+
+
+tablasMultiplicar()
+
+
+def notaFinal():
+    notaAcumulado = 0
+    num = int(input("Ingrese el numero de estudiantes que desea tomar la nota: ")) + 1
+    for i in range(1, num):
+        notaAcumulado += float(input(f"Ingrese el nota del estudiante {i}: "))
+    num = num - 1
+    print(f"{notaAcumulado / num:.2f}")
+
+
+notaFinal()
